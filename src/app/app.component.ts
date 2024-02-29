bdef54621c2647beccbe5cae72b3be9415d52ca6
rommuloifrn <roomulo.cavalkant@outlook.com>
2024-02-29 17:37:17 -0300

    first!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,10 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  //templateUrl: './app.component.html',
+  template: `<h1>Hello worudo!</h1>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'croni';
+  title = 'cronos';
 }
