351f1923e0672ce25e5d982f9f016f4ee636bbc9
rommuloifrn <roomulo.cavalkant@outlook.com>
2024-03-22 15:10:56 -0300

    style: removed unused imports

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable, Subscription, catchError } from 'rxjs';
+import { Subscription } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
