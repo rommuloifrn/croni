a0a30ba71e4325a7450691f338e392f86e8a4494
rommuloifrn <roomulo.cavalkant@outlook.com>
2024-03-27 19:55:10 -0300

    feat: Croni button in topbar now redirects to project list

--- src/app/components/topbar/topbar.component.ts
+++ src/app/components/topbar/topbar.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    CommonModule
+  ],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.css'
 })
 export class TopbarComponent {
-
+  isThereAToken = localStorage.getItem("JWT");
 }
